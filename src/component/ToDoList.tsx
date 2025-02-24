aa9a757c7a88815d05694a61e4c4b6e2d17e0cfb
nsj9164 <nsj9164@gmail.com>
2025-02-24 23:03:47 +0900

    ✨ Add: #6.14~#6.19 강의 코드 추가 - state의 immutability - Recoil selector

--- src/component/ToDoList.tsx
+++ src/component/ToDoList.tsx
@@ -1,21 +1,28 @@
 import { useRecoilState, useRecoilValue } from "recoil";
-import { toDoState } from "../atom";
+import { Categories, categoryState, toDoSelector, toDoState } from "../atom";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value as any); // categories 타입을 받아야하는데 string이야
+  };
+  console.log(category);
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
+      <select value={category} onInput={onInput}>
+        <option value={Categories.TO_DO}>TO DO</option>
+        <option value={Categories.DOING}>DOING</option>
+        <option value={Categories.DONE}>DONE</option>
+      </select>
       <CreateToDo />
-      <ul>
-        {toDos.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
     </div>
   );
 }
